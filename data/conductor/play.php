45fa8c29d138ec4e399e73d4170cfa7b35986709
Charles hutchins <celesteh@gmail.com>
2016-06-22 05:18:36 +0100

    array mangling

--- data/conductor/play.php
+++ data/conductor/play.php
@@ -27,7 +27,7 @@ function write_php_ini($array, $file)
 {
     $res = array();
     foreach ($array as $title => $section)
-      $res[] = "[".$title."]";
+      $res[] = "[$title]";
       foreach($section as $key => $val)
       {
           if (empty($_POST[$key])) {} else {
