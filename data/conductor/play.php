7ddcbb3587800889752337db2e60a629721ec92e
Charles hutchins <celesteh@gmail.com>
2016-06-23 17:36:34 +0100

    trying again

--- data/conductor/play.php
+++ data/conductor/play.php
@@ -66,13 +66,8 @@ $line = $cmd . " " . $args
 
 function LaunchBackgroundProcess($command){
   // Run command Asynchroniously (in a separate thread)
-  if(PHP_OS=='WINNT' || PHP_OS=='WIN32' || PHP_OS=='Windows'){
-    // Windows
-    $command = 'start "" '. $command;
-  } else {
     // Linux/UNIX
-    $command = $command .' > /dev/null &';
-  }
+  $command = $command .' > /dev/null &';
   $handle = popen($command, 'r');
   if($handle!==false){
     pclose($handle);
