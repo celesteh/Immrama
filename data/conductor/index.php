344ca0ddccb1dee446cc6150c8657822d0190074
Charles hutchins <celesteh@gmail.com>
2016-09-25 00:30:08 +0100

    avoid reserved word and move script

--- data/conductor/index.php
+++ data/conductor/index.php
@@ -8,32 +8,6 @@ $conf = parse_ini_file("config.ini");
 <head>
   <link rel="stylesheet" href="/style.css" type="text/css" />
   <link rel="stylesheet" href="/color.css" type="text/css" />
-  <SCRIPT LANGUAGE="javascript">
-<!--
-function OnChange(form)
-{
-    var val  = form.size.value
-    if(val=="prev")
-    {
-      form.width.value= <?php echo $conf['width']; ?>
-      form.height.value= <?php echo $conf['height']; ?>
-
-    } else {
-      if (val=="phone")
-      {
-        form.width.value= 640
-        form.height.value= 960
-
-      } else { //must be tablet
-        form.width.value= 1024
-        form.height.value= 768        
-      }
-    }
-
-    return true;
-}
-//-->
-</SCRIPT>
   <title>Immrama</title>
 </head>
 <body>
@@ -58,12 +32,14 @@ function OnChange(form)
     <input type="number" name="init_sleep" value=<?php echo $conf['init_sleep']; ?>>
   </p>
       <p>
-        <label for="size">Target Screen Geometry</label>
-        <select name="size" onchange='OnChange(this.form);'>
+        <label for="score_size">Target Screen Geometry</label>
+        <select name="score_size" onchange='OnChange(this.form);'>
           <option value="prev">Same as last time (<?php echo $conf['width']; ?>x<?php echo $conf['height']; ?>)</option>
           <option value="phone">Phones (2x3 ratio)</option>
           <option value="tablet">Tablets (4x3 ratio)</option>
         </select>
+      </p>
+      <p>
   <label for ="width">Image width:</label>
     <input type="number" name="width" value=<?php echo $conf['width']; ?>>
 
@@ -71,6 +47,32 @@ function OnChange(form)
     <input type="number" name="height" value=<?php echo $conf['height']; ?>>
   </p>
   <p>
+    <SCRIPT LANGUAGE="javascript">
+  <!--
+  function OnChange(form)
+  {
+      var val  = form.score_size.value
+      if(val=="prev")
+      {
+        form.width.value= <?php echo $conf['width']; ?>
+        form.height.value= <?php echo $conf['height']; ?>
+
+      } else {
+        if (val=="phone")
+        {
+          form.width.value= 640
+          form.height.value= 960
+
+        } else { //must be tablet
+          form.width.value= 1024
+          form.height.value= 768
+        }
+      }
+
+      return true;
+  }
+  //-->
+  </SCRIPT>
 
   <label for ="foreground">Foreground colour:</label>
     <input type="color" name="foreground" value=<?php echo $conf['foreground']; ?>>  </p>
