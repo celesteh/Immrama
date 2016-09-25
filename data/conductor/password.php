c9e74144105d8b507cf90ca4f60c357f4f931540
Charles hutchins <celesteh@gmail.com>
2016-09-25 18:36:56 +0100

    some system admin via web

---
+++ data/conductor/password.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<?php
+$conf = parse_ini_file("config.ini", true);
+
+$cmd = $conf['working']['installation'] . '/wpa_password.py';
+$args = ' '. $_POST['newpass'] . ' ' . $_POST['confirm'];
+
+$success = system($cmd . $args);
+?>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/style.css">
+<link rel="stylesheet" href="/color.css" type="text/css" />
+<title><?php echo $sucess ?></title>
+</head>
+<body>
+<h2><?php echo $sucess ?></h2>
+<ul>
+<li><a href="./">Return to piece settings</a></li>
+<li><a href="advanced.html">Return to Advanced settings</a></li>
+<li><a href="piece.html">View piece as Conductor</a></li>
+<li><a href="../piece.html">View piece as performer</a></li>
+</ul>
+</body>
+</html>
---
+++ data/conductor/shutdown.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<?php
+$conf = parse_ini_file("config.ini", true);
+
+$success = system("sudo halt");
+?>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/style.css">
+<link rel="stylesheet" href="/color.css" type="text/css" />
+<title>Failure</title>
+</head>
+<body>
+<h2>Failed</h2>
+<p>You should not be able to see this page.</p>
+<p>Normally, there will be a short delay before the comuter turns off.
+  Please wait a few moments before unplugging the power supply.
+  The shutdown computer should have a solid red light.
+  Some Pis may reboot instead of shutting down. If this happens,
+  try to unplug the machine while the red light is on.
+</p>
+<p>You should <a href="advanced.html">Try shutting down again</a>.
+<ul>
+<li><a href="./">Return to piece settings</a></li>
+<li><a href="advanced.html">Return to Advanced settings</a></li>
+<li><a href="piece.html">View piece as Conductor</a></li>
+<li><a href="../piece.html">View piece as performer</a></li>
+</ul>
+</body>
+</html>
