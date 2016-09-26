c5fefe6809df91a8a1fa14cefa01ebb5185579ee
Charles hutchins <celesteh@gmail.com>
2016-09-26 20:28:44 +0100

    getting parts fo the password puzzle together

---
+++ wpa_password.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Change the wifi password.')
+parser.add_argument('pass1', nargs='?', default='', help='Password')
+parser.add_argument('pass2', nargs='?', default='', help='Confirm Password')
+args = parser.parse_args()
+
+if pass1 == pass2:
+    # so far so good
+
+    // read the file
+    if pass1 != "":
+        # if there is a password line, replace the first one with the new password
+        print ""
+    # comment out subsequent password lines
+    # save
+    print "Success: Password Changed"
+else :
+    print "Failure: Passwords do not match"
