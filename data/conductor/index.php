5d816264de3e85cf82bc86d45e28d690d19dd34f
Charles hutchins <celesteh@gmail.com>
2016-09-26 14:44:18 +0100

    more testing

--- data/conductor/index.php
+++ data/conductor/index.php
@@ -55,16 +55,16 @@ document.getElementById("imageheight").readonly = true
 var dur = <?php echo $conf['dur']; ?>
 
 var form = document.getElementById("config")
-var input = document.createElement("input") //input element, text
+var input = document.createElement("input") //input element, hidden
 input.setAttribute('type',"hidden")
 input.setAttribute('name',"dur")
 input.setAttribute('value', dur)
 form.insertBefore(input)
 
-var input = document.createElement("input") //input element, text
+var input = document.createElement("input") //input element, number
 input.setAttribute('type',"number")
 input.setAttribute('name',"seconds")
-input.setAttribute('value', dur % 60)
+//input.setAttribute('value', dur % 60)
 form.appendChild(input)
 
 var label = document.createElement("label")
@@ -72,10 +72,10 @@ label.setAttribute('for', "seconds")
 label.htmlFor = "Seconds"
 form.insertBefore(label)
 
-var input = document.createElement("input") //input element, text
+var input = document.createElement("input") //input element, number
 input.setAttribute('type',"number")
 input.setAttribute('name',"minutes")
-input.setAttribute('value', dur / 60)
+//input.setAttribute('value', dur / 60)
 form.insertBefore(input)
 
 var label = document.createElement("label")
