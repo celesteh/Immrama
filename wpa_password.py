5055ae74057cceb9b5a3bce0afce7d57ba461077
Charles hutchins <celesteh@gmail.com>
2016-09-27 14:09:36 +0100

    since there are a bunch of tings to look for, is now oop

--- wpa_password.py
+++ wpa_password.py
@@ -17,6 +17,34 @@ for proc in psutil.process_iter():
 
 setproctitle.setproctitle(PROCNAME)
 
+
+class lineMangler:
+    """Hold stuff for doing pattern matching"""
+
+    def __init__(self, regex, newline):
+        if isinstance(regex, basestring):
+            regex = re.compile(regex)
+        self.regex = regex
+        self.newline = newline
+        self.found = False
+
+    def findreplace(line, shouldReplace):
+        if self.regex.match(line):
+            if (shouldReplace and (not self.found)):
+                line = self.newline
+                self.found = True
+            else:
+                line = '# ' + line
+
+        return line
+
+    def missingLine():
+        if not self.found:
+            self.found = True
+            return self.newline
+        else:
+            return ""
+
 #holy shit do not do this from a command line
 #parser = argparse.ArgumentParser(description='Change the wifi password.')
 #parser.add_argument('pass1', nargs='?', default='', help='Password')
@@ -50,41 +78,49 @@ with open pass_file as f:
 passwd = passwd.rstrip()
 length = len(passwd)
 
+
+
 if (length == 0 || ((length >=8) && (length <=63) )):
-    check = re.compile('\w+')
-        if check.match(passwd):
-            # so far so good
+    # adequate length
+    check = re.compile('\w+') #letters and numbers
+    if check.match(passwd):
+        # so far so good
 
-            searching = False
-            if passwd != "": #Non blank password
-                searching=True
+        havePass = False
+        if passwd != "": #Non blank password
+            havePass=True
 
-            pass_line = re.compile('$wpa_passphrase=\w+')
+        pass_obj = lineMangler('$wpa_passphrase=\w+',
+        'wpa_passphrase='+passwd)
+        wpa2_obj = lineMangler('$wpa=[0-9]', 'wpa=2')
+        key__obj = lineMangler('$wpa_key_mgmt=\w+','wpa_key_mgmt=WPA-PSK')
 
-            with open('/tmp/hostapd.conf', 'w') as result: #file to write
-                with open('/etc/hostapd/hostapd.conf') as conf: #original file
-                    for line in conf:
-                        match = pass_line.match(line)
-                        if match:
-                            # found a password line
-                            if searching:
-                                #replace with our password
-                                line = "wpa_passphrase=" + passwd +'\n'
-                                searching = False
-                            else :
-                                #comment out the password line
-                                line = "# " + line
+        with open('/tmp/hostapd.conf', 'w') as result: #file to write
+            with open('/etc/hostapd/hostapd.conf') as conf: #original file
+                for line in conf:
+                    line = line.rstrip()
 
-                        result.write(line)
+                    line = pass_obj.findreplace(line, havePass)
+                    line = wpa2_obj.findreplace(line, havePass)
+                    line = key_obj.findreplace(line, havePass)
+                    result.write(line+'\n')
 
-                    # end for
-                #end with
-                if searching:
-                    #we did not find a password line in the file
-                    result.write("wpa_passphrase=" + passwd +'\n')
+                # end for
             #end with
-            # copy file to real location
-            #shutil.copy('/tmp/hostapd.conf', '/etc/hostapd/hostapd.conf')
+            if havePass:
+                #make sure we get our lines into the file
+                result.write(pass_obj.missingLine() + '\n')
+                result.write(wpa2_obj.missingLine() + '\n')
+                result.write(key_obj.missingLine() + '\n')
+        #end with
+        # copy file to real location
+        #shutil.copy('/tmp/hostapd.conf', '/etc/hostapd/hostapd.conf')
+        #os.system('etc/init.d/hostapd restart')
+        if havePass:
             print "Success: Password Changed"
+        else:
+            print "Success: Wifi is passwordless"
+    else:
+        print "Failure: Password not set"
 else :
     print "Failure: Password not set"
