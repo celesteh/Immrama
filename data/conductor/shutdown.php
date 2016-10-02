27293881743d398d69fdbd5ca28b1c066af012fb
Charles hutchins <celesteh@gmail.com>
2016-10-02 22:22:25 +0100

    shutdown is wonky

--- data/conductor/shutdown.php
+++ data/conductor/shutdown.php
@@ -8,7 +8,7 @@ $timestamp=date_timestamp_get($date);
 if ($_POST['timestamp']){
   if (($timestamp - $_POST['timestamp']) <= 60000) {
     // 1 minute
-    $success = system("sudo /sbin/halt");
+    //$success = system("sudo /sbin/halt");
   }
 }
 ?>
@@ -39,6 +39,15 @@ if ($_POST['timestamp']){
   <p>time stamp now: <?php echo $timestamp ?></p>
   <p>time at request: <?php echo $_POST['timestamp'] ?></p>
   <p>diff: <?php echo ($timestamp - _POST['timestamp'])?></p>
+  <p><?php
+  if (($timestamp - $_POST['timestamp']) <= 60000) {
+      // 1 minute
+      //$success = system("sudo /sbin/halt");
+      echo "should shut down";
+    } else {
+      echo "should not shutdown";
+    }
+?></p>
   <!--
 <ul>
 <li><a href="./">Return to piece settings</a></li>
