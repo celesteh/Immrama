43a5a2ece580117a9742cf5ad419e290eb5e7875
Charles hutchins <celesteh@gmail.com>
2017-07-01 22:29:24 +0100

    theoretically, this should contain a text editor

---
+++ data/conductor/setintro.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../style.css">
+<link rel="stylesheet" href="../color.css" type="text/css" />
+<title>Set front page text</title>
+</head>
+<body>
+<div id="words">
+  <div class="dropdown">
+    <button class="dropbtn">&#8801;</button>
+   <div class="dropdown-content">
+     <a href="../">Home</a>
+     <a href="./">(Re)Start Piece</a>
+     <a href="./end.php">Stop Piece</a>
+    <a href="./piece.html">Show Piece as Conductor</a>
+    <a href="../piece.html">Show piece as Player</a>
+    <a href="../wifi.php">Show WiFi Password</a>
+    <a href="./requestshutdown.php">Shut down computer</a>
+    <a href="./setintro.php">Set introductory text</a>
+  </div>
+</div>
+<main class="main">
+<script>
+var simplemde = new SimpleMDE();
+</script>
+</main>
+    </div>
+  </body>
+  </html>
