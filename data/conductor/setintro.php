eda37256eff6fca10ab9ef713faa954f5d8d0eea
Charles hutchins <celesteh@gmail.com>
2017-07-02 20:22:25 +0100

    missing a comma

--- data/conductor/setintro.php
+++ data/conductor/setintro.php
@@ -55,7 +55,7 @@ try{
   new SimpleMDE({
   		element: document.getElementById("inputtext"),
       spellChecker: false,
-      <? php if($newfile){ echo "placeholder: \"## Header\n\nText...\"";} ?>
+      <? php if($newfile){ echo "placeholder: \"## Header\n\nText...\",\n";} ?>
       toolbar: [
         {
   		      name: "heading",
